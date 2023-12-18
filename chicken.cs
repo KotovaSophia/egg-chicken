869e8301d9ede55b48b18dfa50c09094785648e2
KotovaSophia <145443920+KotovaSophia@users.noreply.github.com>
2023-12-18 18:17:58 +0300

    Add files via upload

---
+++ chicken.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+internal class Program
+{
+    static void ChickenThread()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Яйцо");
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Thread chicken = new Thread(ChickenThread);
+        chicken.Start();
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Курица");
+        }
+        ThreadAnswer(chicken);
+    }
+
+    static void ThreadAnswer(Thread chicken)
+    {
+        bool ans = chicken.IsAlive;
+        Console.WriteLine(ans ? "Спор Окончен. Победило Яйцо." : "Спор окончен. Победила Курица.");
+    }
+}
